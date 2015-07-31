8f7731648d8a3645b8e36e5582c32c22929e0d7a
chaitanya <vedantamskc@gmail.com>
2015-07-31 12:59:38 +0530

    EmiCalc Design Completed

--- AnyService/app/src/main/java/com/niharinfo/anyservice/FragmentEmiCalculator.java
+++ AnyService/app/src/main/java/com/niharinfo/anyservice/FragmentEmiCalculator.java
@@ -17,6 +17,7 @@ import android.widget.SeekBar;
 
 import com.niharinfo.anyservice.Helper.DecimalDigitsInputFilter;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 /**
@@ -24,9 +25,9 @@ import java.text.DecimalFormat;
  */
 public class FragmentEmiCalculator extends Fragment {
 
-    SeekBar seekBar,seekBarPercentage;
+    SeekBar seekBar,seekBarPercentage,seekbarTimer;
     //FloatSeekBar seekBarPercentage;
-    EditText edtEmiLoanAmount,edtRateOfInterest;
+    EditText edtEmiLoanAmount,edtRateOfInterest,edtLoanTenure;
     String i;
     String r;
 
@@ -45,11 +46,55 @@ public class FragmentEmiCalculator extends Fragment {
         View v = inflater.inflate(R.layout.fragment_emi_calculator,container,false);
         seekBar = (SeekBar)v.findViewById(R.id.seekBar1);
         seekBarPercentage = (SeekBar)v.findViewById(R.id.seekBarROI);
+        seekbarTimer = (SeekBar)v.findViewById(R.id.seekBarTimer);
         seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
         seekBarPercentage.setOnSeekBarChangeListener(seekBarChangeListenerROI);
+        seekbarTimer.setOnSeekBarChangeListener(seekBarChangeListenerMonths);
         edtRateOfInterest = (EditText)v.findViewById(R.id.edtEmiLoanROI);
         edtEmiLoanAmount = (EditText)v.findViewById(R.id.edtEmiLoanAmount);
+        edtLoanTenure = (EditText)v.findViewById(R.id.edtLoanTenure);
+
+        //Calculation
+        double amount=1000.0,Rate;
+        int duration1=2;
+        Rate = (2.0/12.0)/100;
+        double powerVal = Math.pow((1+Rate),duration1);
+        double numerator = amount*Rate*powerVal;
+        double denom = powerVal-1;
+        double total =  numerator/denom;
+        double t = total;
+        BigDecimal b = new BigDecimal(t).setScale(2,BigDecimal.ROUND_HALF_UP);
+
         edtRateOfInterest.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(3,1)});
+        edtLoanTenure.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                edtLoanTenure.setSelection(count);
+                if(count==0){
+                    i="0";
+                }else{
+                    i = s.toString();
+                }
+                int amt = Integer.parseInt(i);
+                if(amt>360){
+                    seekbarTimer.setProgress(360);
+                    edtLoanTenure.setText("360");
+                }else{
+                    seekbarTimer.setProgress(Integer.parseInt(i));
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         edtRateOfInterest.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -114,25 +159,31 @@ public class FragmentEmiCalculator extends Fragment {
         return v;
     }
 
-    /*private void updateBackground()
-    {
-        seekBar = seekBar.getProgress();
-        *//**//*seekG = greenSeekBar.getProgress();
-        //seekB = blueSeekBar.getProgress();*//**//*
-        mScreen.setBackgroundColor(
-                0xff000000
-                        + seekR * 0x10000
-                        + seekG * 0x100
-                        + seekB
-        );
-    }*/
-
     private SeekBar.OnSeekBarChangeListener seekBarChangeListener =
             new SeekBar.OnSeekBarChangeListener() {
                 @Override
                 public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                    // onProgressChanged(seekBar,progress,fromUser);
                     edtEmiLoanAmount.setText(String.valueOf(progress));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            };
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListenerMonths =
+            new SeekBar.OnSeekBarChangeListener(){
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    edtLoanTenure.setText(String.valueOf(progress));
                 }
 
                 @Override
@@ -165,39 +216,4 @@ public class FragmentEmiCalculator extends Fragment {
 
                 }
             };
-
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-        // get quantity for plurals string
-        int quantity = setQuantity(progress);
-
-        // convert the actual progress to float
-        float floatProgress = convertProgress(progress);
-
-        // get the actual string and replace formatting with the float value
-        String currentProgress = getResources()
-                .getQuantityString(R.plurals.seekBarProgress, // get the plurals
-                        quantity, // set the quantity
-                        floatProgress); // format arguments
-
-        // set text to display
-        edtRateOfInterest.setText(currentProgress);
-
-    }
-
-    private Float convertProgress(int progress){
-        return ((Float.valueOf(String.valueOf(progress)))/(float)10);
-    }
-
-    private int setQuantity(int progress){
-        int quantity;
-
-        if (((progress%10) == 0) && ((progress/10) == 1)){
-            quantity = 1;
-        } else {
-            quantity = 2;
-        }
-
-        return quantity;
-    }
 }
